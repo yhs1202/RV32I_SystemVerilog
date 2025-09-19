4702c1777c1d27b5249b0845a4bfa25ada1bbb73
hsyoon_kccistc <dbsghtmd99@gmail.com>
2025-09-19 16:48:16 +0900

    more R-type instruction logic added (cont'd)

--- src/alu_32bit.sv
+++ src/alu_32bit.sv
@@ -1,6 +1,7 @@
 `timescale 1ns/1ps
 module alu_32bit (
     input logic [2:0] ALUOp,
+    input logic [3:0] ALUControl,
     input logic [31:0] a,
     input logic [31:0] b,
 
@@ -8,22 +9,52 @@ module alu_32bit (
     output logic [31:0] result
 );
 
-    enum logic [2:0] {
-        ADD, SUB, AND, OR, SLT
-      } operation;
+    enum logic [3:0] {
+        ADD = 4'b0000, 
+        SUB = 4'b0001, 
+        XOR = 4'b0010, 
+        OR  = 4'b0011, 
+        AND = 4'b0100, 
+        SLL = 4'b0101, 
+        SRL = 4'b0110, 
+        SRA = 4'b0111, 
+        SLT = 4'b1000, 
+        SLTU = 4'b1001
+    } operation;
 
     assign N = result[31];
     assign Z = (result == 32'b0);
-    // assign V = 
+    assign V = (a[31] == b[31]) && (result[31] != a[31]);  // Carry in != Carry out for MSB
 
-    always_comb begin
+    always_comb begin : ALU_operations
+        // Determine operation based on ALUOp and ALUControl
         case (ALUOp)
-            ADD: {C, result} = a + b;
-            SUB: {C, result} = a - b;
-            AND: result = a & b;
-            OR: result = a | b;
-            SLT: result = {31'b0, a < b};
-            default: result = 32'bx;    // NO OPERATION
+            3'b000: begin   // Load/Store
+            end
+            3'b001: begin   // Branch
+            end
+            3'b010: begin   // R-type
+                case (ALUControl)
+                    ADD: {C, result} = a + b;
+                    SUB: {C, result} = a - b;
+                    XOR: result = a ^ b;
+                    OR: result = a | b;
+                    AND: result = a & b;
+                    SLL: result = a << b[4:0];
+                    SRL: result = a >> b[4:0];
+                    SRA: result = a >>> b[4:0];
+                    SLT: result = {31'b0, a < b};
+                    SLTU: result = {31'b0, $unsigned(a) < $unsigned(b)};
+                    default: result = 32'bx;    // NO OPERATION
+                endcase
+            end
+            /*
+            3'b011: begin   // I-type
+            end
+            3'b100: begin   // JAL
+            end
+            */
+            default: result = 32'bx; // NO OPERATION
         endcase
         
     end
--- src/control_unit.sv
+++ src/control_unit.sv
@@ -6,7 +6,8 @@ module control_unit (
     input logic [31:0] instruction_code,
 
     output logic Reg_write,
-    output logic [2:0] ALUOp
+    output logic [2:0] ALUOp,
+    output logic [3:0] ALUControl
 );
 
     logic [6:0] func7;
@@ -16,36 +17,59 @@ module control_unit (
     assign func3 = instruction_code[14:12];
     assign opcode = instruction_code[6:0];
 
-    always_comb begin : blockName
+    // Decode instruction
+    always_comb begin : opcode_decoder
         case (opcode)
-            7'b011_0011: begin // R-type
-                case ({func7, func3})
-                    10'b0000000_000: ALUOp = 3'b000; // ADD
-                    10'b0100000_000: ALUOp = 3'b001; // SUB
-                    10'b0000000_111: ALUOp = 3'b010; // AND
-                    10'b0000000_110: ALUOp = 3'b011; // OR
-                    10'b0000000_010: ALUOp = 3'b100; // SLT
-                    default: ALUOp = 3'bxxx; // Default to ADD
+            7'b000_0011: ALUOp = 3'b000; // Load
+            7'b010_0011: ALUOp = 3'b000; // Store
+            7'b110_0011: ALUOp = 3'b001; // Branch
+            7'b011_0011: ALUOp = 3'b010; // R-type
+            7'b001_0011: ALUOp = 3'b011; // I-type
+            7'b110_1111: ALUOp = 3'b100; // JAL
+            default:    ALUOp = 3'b111; // NOP/illegal
+    endcase
+    end
+
+    always_comb begin : ALU_decoder
+        unique case (ALUOp)
+            3'b000: begin // Load/Store
+            end
+            3'b001: begin // Branch
+            end
+            3'b010: begin // R-type
+                unique case ({func7, func3})
+                    10'b0000000_000: ALUControl = 4'b0000; // ADD
+                    10'b0100000_000: ALUControl = 4'b0001; // SUB
+                    10'b0000000_100: ALUControl = 4'b0010; // XOR
+                    10'b0000000_110: ALUControl = 4'b0011; // OR
+                    10'b0000000_111: ALUControl = 4'b0100; // AND
+                    10'b0000000_001: ALUControl = 4'b0101; // SLL
+                    10'b0000000_101: ALUControl = 4'b0110; // SRL
+                    10'b0100000_101: ALUControl = 4'b0111; // SRA
+                    10'b0000000_010: ALUControl = 4'b1000; // SLT
+                    10'b0000000_011: ALUControl = 4'b1001; // SLTU
+                    default: ALUControl = 4'bxxxx; // Default to ADD
                 endcase
                 Reg_write = 1'b1;
             end
             /*
-            7'b0000_011: begin // Load
+            3'b011: begin // I-type
+                unique case (func3)
+                    3'b000: ALUControl = 4'b0000; // ADDI
+                    3'b010: ALUControl = 4'b1000; // SLTI
+                    3'b110: ALUControl = 4'b0110; // ORI
+                    default: ALUControl = 4'bxxxx; // Default to ADDI
+                endcase
                 Reg_write = 1'b1;
-                ALUOp = 3'b000;
             end
-            7'b0100_011: begin // Store
-                Reg_write = 1'b0;
-                ALUOp = 3'b000;
-            end
-            default: begin
-                Reg_write = 1'b0;
-                ALUOp = 3'b000;
+            3'b100: begin // JAL
+                ALUControl = 4'bxxxx; // No ALU operation needed
+                Reg_write = 1'b1; // Write return address to rd
             end
             */
-            default: begin
+            default: begin // NOP/illegal
+                ALUControl = 4'bxxxx;
                 Reg_write = 1'b0;
-                ALUOp = 3'b111;
             end
         endcase
     end
