62bf827f0f774f0e179da91c4aaa5d2b8938d362
hsyoon_ser9 <dbsghtmd99@naver.com>
2025-10-15 11:51:19 +0900

    Update array init comment to ensure full memory allocation

--- test_c/swap.c
+++ test_c/swap.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
 void swap (int *a, int *b);
 
 int main () {
-    int arr[6] = {4, 1, 0, 2, 3};   // 5 -> 6
+    // Changed arr[5] -> arr[6] to force compiler to generate full initialization
+    // Ensures all array elements are explicitly stored in memory
+    int arr[6] = {4, 1, 0, 2, 3};   
     int size = 5;
     for (int i = 0; i < size-1; i++) {
         for (int j = 0; j < size-i-1; j++) {
